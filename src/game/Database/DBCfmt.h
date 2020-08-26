ef9ac5949c1d0fb73cc14d0467f6a17477d1487f
tbcmangos <tbc.mangos@gmail.com>
2020-08-26 15:43:37 +0800

    update DBCfmt and enums

--- src/game/Database/DBCfmt.h
+++ src/game/Database/DBCfmt.h
@@ -21,7 +21,97 @@
 
 #ifndef MANGOS_DBCSFRM_H
 #define MANGOS_DBCSFRM_H
-
+#if SUPPORTED_CLIENT_BUILD > CLIENT_BUILD_1_12_1
+const char AreaTableEntryfmt[] = "iiinixxxxxissssssssssssssssxiiiiixx";
+const char AreaTriggerEntryfmt[] = "niffffffff";
+const char AuctionHouseEntryfmt[] = "niiixxxxxxxxxxxxxxxxx";
+const char BankBagSlotPricesEntryfmt[] = "ni";
+char const CharSectionsEntryfmt[] = "diiiiixxxi";
+char const CharacterFacialHairStylesfmt[] = "iiixxxxxxxx";
+const char BattlemasterListEntryfmt[] = "niiiiiiiiiiiixxssssssssssssssssxx";
+const char CharStartOutfitEntryfmt[] = "diiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxxxx";
+// 3*12 new item fields in 3.0.x
+// const char CharStartOutfitEntryfmt[]="diiiiiiiiiiiiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const char CharTitlesEntryfmt[] = "nxssssssssssssssssxxxxxxxxxxxxxxxxxxi";
+const char ChatChannelsEntryfmt[] = "iixssssssssssssssssxxxxxxxxxxxxxxxxxx";
+// ChatChannelsEntryfmt, index not used (more compact store)
+const char ChrClassesEntryfmt[] = "nxixssssssssssssssssxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxix";
+const char ChrRacesEntryfmt[] = "nxixiixxixxxxissssssssssssssssxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxi";
+const char CinematicCameraEntryfmt[] = "nsiffff";
+const char CinematicSequencesEntryfmt[] = "nxixxxxxxx";
+const char CreatureDisplayInfofmt[] = "nixifxxxxxxxxx";
+const char CreatureDisplayInfoExtrafmt[] = "nixxxxxxxxxxxxxxxxxxx";
+const char CreatureFamilyfmt[] = "nfifiiiissssssssssssssssxx";
+const char CreatureModelDatafmt[] = "nixxfxxxxxxxxxxffxxxxxxx";
+const char CreatureSpellDatafmt[] = "niiiixxxx";
+const char CreatureTypefmt[] = "nxxxxxxxxxxxxxxxxxx";
+const char DurabilityCostsfmt[] = "niiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+const char DurabilityQualityfmt[] = "nf";
+const char EmotesEntryfmt[] = "nxxiiix";
+const char EmotesTextEntryfmt[] = "nxixxxxxxxxxxxxxxxx";
+// const char FactionEntryfmt[] = "niiiiiiiiiiiiiiiiiissssssssssssssssxxxxxxxxxxxxxxxxxx";
+const char FactionTemplateEntryfmt[] = "niiiiiiiiiiiii";
+const char GameObjectDisplayInfofmt[] = "nsxxxxxxxxxxffffff";
+const char GMSurveyCurrentSurveyfmt[] = "ni";
+const char GMSurveySurveysfmt[] = "niiiiiiiiii";
+const char GMSurveyQuestionsfmt[] = "nssssssssssssssssx";
+const char GMTicketCategoryfmt[] = "nssssssssssssssssx";
+const char GemPropertiesEntryfmt[] = "nixxi";
+const char GtCombatRatingsfmt[] = "f";
+const char GtChanceToMeleeCritBasefmt[] = "f";
+const char GtChanceToMeleeCritfmt[] = "f";
+const char GtChanceToSpellCritBasefmt[] = "f";
+const char GtChanceToSpellCritfmt[] = "f";
+const char GtOCTRegenHPfmt[] = "f";
+const char GtNPCManaCostScalerfmt[] = "f";
+// const char GtOCTRegenMPfmt[]="f";
+const char GtRegenHPPerSptfmt[] = "f";
+const char GtRegenMPPerSptfmt[] = "f";
+const char Itemfmt[] = "niii";
+const char ItemBagFamilyfmt[] = "nxxxxxxxxxxxxxxxxx";
+const char ItemClassfmt[] = "nxxssssssssssssssssx";
+// const char ItemDisplayTemplateEntryfmt[]="nxxxxxxxxxxixxxxxxxxxxx";
+// const char ItemCondExtCostsEntryfmt[]="xiii";
+const char ItemExtendedCostEntryfmt[] = "niiiiiiiiiiiii";
+const char ItemRandomPropertiesfmt[] = "nxiiixxssssssssssssssssx";
+const char ItemRandomSuffixfmt[] = "nssssssssssssssssxxiiiiii";
+const char ItemSetEntryfmt[] = "dssssssssssssssssxxxxxxxxxxxxxxxxxxiiiiiiiiiiiiiiiiii";
+const char LiquidTypefmt[] = "niii";
+const char LockEntryfmt[] = "niiiiiiiiiiiiiiiiiiiiiiiixxxxxxxx";
+const char MailTemplateEntryfmt[] = "nxxxxxxxxxxxxxxxxxssssssssssssssssx";
+const char MapEntryfmt[] = "nxixssssssssssssssssxxxxxxxixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxiffiixxi"; //reading in from map_template, not in use
+char const NamesProfanityEntryfmt[] = "dsi";
+char const NamesReservedEntryfmt[] = "dsi";
+const char QuestSortEntryfmt[] = "nxxxxxxxxxxxxxxxxx";
+const char RandomPropertiesPointsfmt[] = "niiiiiiiiiiiiiii";
+const char SkillLinefmt[] = "nixssssssssssssssssxxxxxxxxxxxxxxxxxxi";
+const char SkillLineAbilityfmt[] = "niiiixxiiiiixxi";
+const char SkillRaceClassInfofmt[] = "diiiiiix";
+char const SpellCategoryfmt[] = "ni";
+const char SkillTiersfmt[] = "niiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+const char SoundEntriesfmt[] = "nxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const char SpellCastTimefmt[] = "niii";
+const char SpellDurationfmt[] = "niii";
+const char SpellFocusObjectfmt[] = "nxxxxxxxxxxxxxxxxx";
+const char SpellItemEnchantmentfmt[] = "niiiiiixxxiiissssssssssssssssxiiii";
+const char SpellItemEnchantmentConditionfmt[] = "nbbbbbxxxxxbbbbbbbbbbiiiiiXXXXX";
+const char SpellRadiusfmt[] = "nfxx";
+const char SpellRangefmt[] = "nffixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const char SpellShapeshiftfmt[] = "nxxxxxxxxxxxxxxxxxxiixiixxxiiiiiiii";
+const char SpellVisualfmt[] = "nxxxxxiixxxxxxxxxxxxxxxxx"; // wotlk - "nxxxxxxiixxxxxxxxxxxxxxxxxxxxxxx"
+const char StableSlotPricesfmt[] = "ni";
+const char SummonPropertiesfmt[] = "niiiii";
+const char TalentEntryfmt[] = "niiiiiiiixxxxixxixxxi";
+const char TalentTabEntryfmt[] = "nxxxxxxxxxxxxxxxxxxxiix";
+const char TaxiNodesEntryfmt[] = "nifffssssssssssssssssxii";
+const char TaxiPathEntryfmt[] = "niii";
+const char TaxiPathNodeEntryfmt[] = "diiifffiiii";
+const char TotemCategoryEntryfmt[] = "nxxxxxxxxxxxxxxxxxii";
+const char WMOAreaTableEntryfmt[] = "niiixxxxxiixxxxxxxxxxxxxxxxx";
+const char WorldMapAreaEntryfmt[] = "xinxffffi";
+// const char WorldMapOverlayEntryfmt[]="nxiiiixxxxxxxxxxx";
+const char WorldSafeLocsEntryfmt[] = "nifffxxxxxxxxxxxxxxxxx";
+#else
 char const AreaTableEntryfmt[]="niiiixxxxxissssssssxixxxi";
 char const AreaTriggerEntryfmt[]="niffffffff";
 char const AuctionHouseEntryfmt[]="niiixxxxxxxxx";
@@ -60,7 +150,7 @@ char const ItemSetEntryfmt[] = "dssssssssxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 char const LiquidTypefmt[]="niii";
 char const LockEntryfmt[]="niiiiiiiiiiiiiiiiiiiiiiiixxxxxxxx";
 char const MailTemplateEntryfmt[]="nxxxxxxxxx";
-char const MapEntryfmt[]="nxixssssssssxxxxxxxixxxxxxxxxxxxxxxxxxixxx";
+char const MapEntryfmt[]="nxixssssssssxxxxxxxixxxxxxxxxxxxxxxxxxixxx"; //reading in from map_template, not in use
 char const NamesProfanityEntryfmt[] = "ds";
 char const NamesReservedEntryfmt[] = "ds";
 char const QuestSortEntryfmt[]="nxxxxxxxxx";
@@ -85,5 +175,6 @@ char const WMOAreaTableEntryfmt[]="niiixxxxxiixxxxxxxxx";
 char const WorldMapAreaEntryfmt[]="xinxffff";
 //char const WorldMapOverlayEntryfmt[]="nxiiiixxxxxxxxxxx";
 char const WorldSafeLocsEntryfmt[]="nifffxxxxxxxxx";
+#endif
 
 #endif
